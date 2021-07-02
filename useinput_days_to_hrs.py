26b9ff523202ad31cea8a9627f71deb2da09a2a0
anokhramesh <67797819+anokhramesh@users.noreply.github.com>
2021-07-02 23:36:05 +0400

    Add files via upload

---
+++ useinput_days_to_hrs.py
@@ -0,0 +1,10 @@
+# A program for convert User inputed number of  days to Hours.
+calculation_to_units = 24
+name_of_unit = "Hours"
+
+def days_to_units(num_of_days):
+    return(f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}")
+user_input = input("Enter the days for convert to Hrs\n ")
+user_input_number = int(user_input)
+calculated_value = days_to_units(user_input_number)
+print(calculated_value)
